5b52b425097d72360f50218117983bd54e3bc77a
Qile0317 <qile0317@gmail.com>
2024-12-09 15:52:38 -0800

    another gh action fix attempt

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.GITHUB_ACTIONS ? '/bioe131-final-project-qja-fa24' : '',
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/bioe131-final-project-qja-fa24' : '',
 };
 
 export default nextConfig;
