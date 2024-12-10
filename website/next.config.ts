eb8479f74da0aea68c2aa368d1ab87cfa6b401a3
Qile0317 <qile0317@gmail.com>
2024-12-09 16:31:05 -0800

    fix?

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = 'bioe131-final-project-qja-fa24';
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true
   },
-  basePath: process.env.GITHUB_ACTIONS ? '/bioe131-final-project-qja-fa24' : '',
-  assetPrefix: process.env.GITHUB_ACTIONS ? '/bioe131-final-project-qja-fa24' : '',
+  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
 };
 
 // export default nextConfig;
