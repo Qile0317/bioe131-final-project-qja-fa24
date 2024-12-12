980e4aba8c63b2a7f3f4eedf12e4ebd01bd1c534
Qile0317 <qile0317@gmail.com>
2024-12-11 22:14:41 -0800

    merge main

--- installation_scripts/fetch_data.sh
+++ installation_scripts/fetch_data.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Please run script with 'source', after setting APACHE_ROOT.
-# This won't work for all systems.
 APACHE_ROOT=${APACHE_ROOT:=/var/www/html}
 
 IFS=$'\n'
+
+metadata_file="genome_data/metadata.csv"
+temp_dir="temp_genome_data"
+mkdir -p "$temp_dir"
+echo "id,country" > "$temp_dir/metadata.csv"
+
 for line in $(cat installation_scripts/data_listing.txt); do
-    # mkdir tmp
-    # cd tmp
     echo "reading $line"
 
     url="${line%%,*}"
@@ -18,32 +21,33 @@ for line in $(cat installation_scripts/data_listing.txt); do
     acc=$(echo "$url" | awk -F'/' '{print $(NF-1)}')
     fna_url="${url}${acc}_genomic.fna.gz"
     gff_url="${url}${acc}_genomic.gff.gz"
-    # Fasta nucleic acid (genome)
 
-    mkdir -p "genome_data/${location}"
-    cd "genome_data/${location}"
+    mkdir -p "$temp_dir/${location}"
+    cd "$temp_dir/${location}"
 
     wget $fna_url -O ${location}.fna.gz
     gunzip ${location}.fna.gz
     samtools faidx ${location}.fna
     bgzip -c ${location}.fna > ${location}.fna.gz
     samtools faidx ${location}.fna.gz
-    # jbrowse add-assembly ${location}.fna --out $APACHE_ROOT/jbrowse2 --load copy
 
-    # Annotation
     wget $gff_url -O ${location}.gff.gz
     gunzip ${location}.gff.gz
     jbrowse sort-gff ${location}.gff > ${location}_genes.gff
     bgzip ${location}_genes.gff
     tabix ${location}_genes.gff.gz
-    # jbrowse add-track ${location}_genes.gff.gz --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames ${location}.fna
-    # jbrowse text-index --out $APACHE_ROOT/jbrowse2
+
+    echo "$acc,$location" >> "../../metadata.csv"
 
     cd ../..
 done
 
+# Move the temp directory to the specified location
 if [ -z "$1" ]; then
-    mv genome_data/ website/public/genome_data/
+    mv "$temp_dir/" website/public/genome_data/
 else
-    mv genome_data/ "$1"
+    mv "$temp_dir/" "$1"
 fi
+
+# Clean up
+rmdir "$temp_dir"
--- website/src/app/components/GenomeBrowser.tsx
+++ website/src/app/components/GenomeBrowser.tsx
@@ -22,14 +22,14 @@ export default function GenomeBrowser({ assemblyName = 'China_(Wuhan).fna' }) {
             name: assemblyName,
             sequence: {
               type: 'ReferenceSequenceTrack',
-              trackId: `Reference Track (${assemblyName})`,
+              trackId: 'Reference Track (' + assemblyName + ')',
               adapter: {
                 type: 'IndexedFastaAdapter',
                 fastaLocation: {
-                  uri: `${process.env.PUBLIC_URL}/genome_data/${loc}/${assemblyName}`,
+                  uri: process.env.NEXT_PUBLIC_BASE_PATH + '/genome_data/' + loc + '/' + assemblyName,
                 },
                 faiLocation: {
-                  uri: `${process.env.PUBLIC_URL}/genome_data/${loc}/${assemblyName}.fai`,
+                  uri: process.env.NEXT_PUBLIC_BASE_PATH + '/genome_data/' + loc + '/' + assemblyName + '.fai',
                 },
               },
             }
@@ -45,11 +45,11 @@ export default function GenomeBrowser({ assemblyName = 'China_(Wuhan).fna' }) {
             adapter: {
               type: 'Gff3TabixAdapter',
               gffGzLocation: {
-                uri: `${process.env.PUBLIC_URL}/genome_data/${loc}/${loc}_genes.gff.gz`,
+                uri: process.env.NEXT_PUBLIC_BASE_PATH + '/genome_data/' + loc + '/' + loc + '_genes.gff.gz',
               },
               index: {
                 location: {
-                  uri: `${process.env.PUBLIC_URL}/genome_data/${loc}/${loc}_genes.gff.gz.tbi`,
+                  uri: process.env.NEXT_PUBLIC_BASE_PATH + '/genome_data/' + loc + '/' + loc + '_genes.gff.gz.tbi',
                 },
               },
             },
